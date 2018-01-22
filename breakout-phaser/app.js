1630d0afbd110aff6263b825aa31691e0c310bc8
aaron <alkrauss48@gmail.com>
2018-01-22 11:59:36 -0600

    Add scaling to phase breakout

--- breakout-phaser/app.js
+++ breakout-phaser/app.js
@@ -5,6 +5,10 @@ var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
 });
 
 function preload() {
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+  game.stage.backgroundColor = '#eee';
 }
 
 function create() {
