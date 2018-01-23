fb9d888bdee66d6799447282979061cd0f4d8ad6
aaron <alkrauss48@gmail.com>
2018-01-22 21:14:14 -0600

    Add the paddle, make it move and collide

--- breakout-phaser/app.js
+++ breakout-phaser/app.js
@@ -5,6 +5,7 @@ var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
 });
 
 var ball;
+var paddle;
 
 function preload() {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -12,16 +13,23 @@ function preload() {
   game.scale.pageAlignVertically = true;
   game.stage.backgroundColor = '#eee';
   game.load.image('ball', 'img/ball.png');
+  game.load.image('paddle', 'img/paddle.png');
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  ball = game.add.sprite(50, 50, 'ball');
+  ball = game.add.sprite(game.world.width * .5, game.world.height - 25, 'ball');
+  paddle = game.add.sprite(game.world.width * .5, game.world.height - 5, 'paddle');
+  paddle.anchor.set(.5, 1);
   game.physics.enable(ball, Phaser.Physics.ARCADE);
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
   ball.body.collideWorldBounds = true;
-  ball.body.velocity.set(150, 150);
+  ball.body.velocity.set(150, -150);
   ball.body.bounce.set(1);
+  paddle.body.immovable = true;
 }
 
 function update() {
+  game.physics.arcade.collide(ball, paddle);
+  paddle.x = game.input.x || game.world.width * .5;
 }
